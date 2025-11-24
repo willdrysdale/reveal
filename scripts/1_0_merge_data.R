e9a3d9b90e892d5d9a79773484fa64c9631a9af1
willdrysdale <wsd500@york.ac.uk>
2025-11-24 12:00:14 +0000

    Add pressure and ND temp to merge

--- scripts/1_0_merge_data.R
+++ scripts/1_0_merge_data.R
@@ -21,7 +21,7 @@ coreDat = mirai({
         filePath, 
         startDate = startDate, 
         endDate = endDate,
-        selectVar = c("LAT_GIN", "LON_GIN", "ALT_GIN"),
+        selectVar = c("LAT_GIN", "LON_GIN", "ALT_GIN", "PS_RVSM", "PS_RVSM_FLAG", "TAT_ND_R"),
         dimensionName = "sps32",
         averageNanoString = "00:00:10"
       ) |> 
